571f0b7ed0ddeb884c9293fe34b8ce0ddfadfd43
Jozef Fulop <jofi@agemsoft.sk>
2013-04-04 11:34:36 +0200

    drobne upravy

--- obedovat.rb
+++ obedovat.rb
@@ -4,21 +4,39 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+def print_title(title)
+  puts '#######################################'
+  puts title
+  puts '#######################################'
+end
+
+def print_content(content)
+  puts content
+  puts
+  puts
+end
+
+LINKY = {"Portus" => 'http://www.portus.sk/lang/sk/restauracia/'}
+
+
 RESTAURACIE = ['http://www.obedovat.sk/bratislava/Restauracia_7172_Beer_Palace',
-               'http://www.obedovat.sk/bratislava/Restauracia_252_Portus',
                'http://www.obedovat.sk/bratislava/Restauracia_344_Dubliner_Irish_pub',
                'http://www.obedovat.sk/bratislava/Restauracia_295_Hacienda_Mexicana',
                'http://www.obedovat.sk/bratislava/Restauracia_6990_All_stars_cafe',
                'http://www.obedovat.sk/bratislava/Restauracia_7218_Sladov%C5%88a_-_House_of_Beer',
                'http://www.obedovat.sk/bratislava/Restauracia_56_Pra%C5%A1n%C3%A1_Ba%C5%A1ta'
 ]
+
+LINKY.each do
+  |nazov, linka|
+  print_title(nazov)
+  print_content(linka)
+end
+
 RESTAURACIE.each do
   |rest|
   doc = Nokogiri::HTML(open(rest))
-  puts '#######################################'
-  doc.css('h1.h2homenews01c').each {|e| puts e.content}
-  puts '#######################################'
-  puts doc.css('.texthelp')[2].content
-  puts
-  puts
+  nazov = doc.css('h1.h2homenews01c').map{|e| e.content}.join(',')
+  print_title(nazov)
+  print_content(doc.css('.texthelp')[2].content)
 end
